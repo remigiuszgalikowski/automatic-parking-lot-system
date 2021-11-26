c4c136ed109d4b3322d74df079e350afa653d50f
remigiuszgalikowski <remigiuszgalikowski@gmail.com>
2021-11-26 16:53:02 +0100

    modified plate recognizer to use file adapter

--- camera-scanner/src/main/java/PlateRecognizer.java
+++ camera-scanner/src/main/java/PlateRecognizer.java
@@ -3,17 +3,35 @@ import net.sf.javaanpr.intelligence.Intelligence;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.io.InputStream;
+
+import net.sf.javaanpr.jar.Main;
 
 public class PlateRecognizer {
 
-    Intelligence intel = new Intelligence();
+    //Intelligence intel = new Intelligence();
+    FileAdapter fileAdapter;
 
     public PlateRecognizer() throws ParserConfigurationException, IOException, SAXException {
+        Main.systemLogic = new Intelligence();
+        fileAdapter = new FileAdapter();
     }
 
-    public String recognize(CarSnapshot carSnap) {
-        String numberPlate = intel.recognize(carSnap);
+    public String recognize(String path) throws IOException {
+        CarSnapshot carSnap = fileAdapter.adapt(path);
+        String numberPlate = Main.systemLogic.recognize(carSnap);
+        return numberPlate;
+    }
+    public String recognize(BufferedImage img) {
+        CarSnapshot carSnap = fileAdapter.adapt(img);
+        String numberPlate = Main.systemLogic.recognize(carSnap);
+        return numberPlate;
+    }
+    public String recognize(InputStream is) throws IOException {
+        CarSnapshot carSnap = fileAdapter.adapt(is);
+        String numberPlate = Main.systemLogic.recognize(carSnap);
         return numberPlate;
     }
 }
